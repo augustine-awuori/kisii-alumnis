5a23cb9da7662aa1cd0198ed8e937b91891fb0be
augustine-awuori <68807737+Augustine95@users.noreply.github.com>
2025-12-05 19:33:03 +0300

    Use real database schools

--- src/components/AlumniRegistrationForm.tsx
+++ src/components/AlumniRegistrationForm.tsx
@@ -19,15 +19,38 @@ const formSchema = z.object({
 type FormData = z.infer<typeof formSchema>;
 
 const schools = [
-  "School of Agriculture and Natural Resources",
-  "School of Arts and Social Sciences",
-  "School of Business and Economics",
-  "School of Education",
-  "School of Engineering and Technology",
-  "School of Health Sciences",
-  "School of Law",
-  "School of Pure and Applied Sciences",
-  "School of Information and Communication Studies",
+  {
+    _id: "693304d4d4dbad14b557ce0d",
+    label: "SCHOOL OF AGRICULTURE AND NATURAL RESOURCES MANAGEMENT",
+  },
+  {
+    _id: "693304e5d4dbad14b557ce0e",
+    label: "SCHOOL OF ARTS AND SOCIAL SCIENCES",
+  },
+  {
+    _id: "693304f4d4dbad14b557ce0f",
+    label: "SCHOOL OF BUSINESS AND ECONOMICS",
+  },
+  {
+    _id: "69330516d4dbad14b557ce10",
+    label: "SCHOOL OF EDUCATION AND HUMAN RESOURCE DEVELOPMENT",
+  },
+  {
+    _id: "6933052ad4dbad14b557ce11",
+    label: "SCHOOL OF HEALTH SCIENCES",
+  },
+  {
+    _id: "6933053cd4dbad14b557ce12",
+    label: "SCHOOL OF INFORMATION SCIENCE & TECHNOLOGY",
+  },
+  {
+    _id: "69330551d4dbad14b557ce13",
+    label: "SCHOOL OF LAW",
+  },
+  {
+    _id: "69330563d4dbad14b557ce14",
+    label: "SCHOOL OF PURE AND APPLIED SCIENCES",
+  },
 ];
 
 const courses = [
@@ -92,7 +115,7 @@ export function AlumniRegistrationForm() {
       />
 
       <FormSelect
-        items={schools.map((sch) => ({ _id: sch, label: sch }))}
+        items={schools}
         onValueSelect={(value) => setValue("school", value)}
         title="School of Study"
         error={errors.school}
